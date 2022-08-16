be4f811c099b0ad38b105b295a5ca1bc676f3a35
Mateusz Mesek <mateusz@mesek.pl>
2022-08-16 19:08:12 +0200

    support DocumentDataEav 0.1.2

--- src/AttributeValueResolver/DefaultResolver.php
+++ src/AttributeValueResolver/DefaultResolver.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types=1);
 
-namespace MateuszMesek\DocumentDataCatalogCategory\Command\GetDocumentNodes;
+namespace MateuszMesek\DocumentDataCatalogCategory\AttributeValueResolver;
 
 use Magento\Eav\Api\Data\AttributeInterface;
 use MateuszMesek\DocumentDataApi\InputInterface;
 use MateuszMesek\DocumentDataCatalogCategory\Data\Input;
 use MateuszMesek\DocumentDataEavApi\AttributeValueResolverInterface;
 
-class AttributeValueResolver implements AttributeValueResolverInterface
+class DefaultResolver implements AttributeValueResolverInterface
 {
     public function resolver(AttributeInterface $attribute, InputInterface $input)
     {
